cecac606d2cc7674304c64d980bb8a11ad5afddf
shamim99apk <rianahmedshamim99apk@gmail.com>
2025-06-01 01:02:48 +0200

    completed design par jght

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import EventList from "@/components/landing/EventList";
+import Header from "@/components/landing/Header";
+
+export default function Home() {
+  return (
+    <section className="container">
+      <Header />
+      <h2>hi</h2>
+      <EventList />
+    </section>
+  );
+}
